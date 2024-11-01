e3f2d002e02f925b2b5b1efce5c7eec5957ca6f8
VALDEIR SAMPAIO <saparavaldeir@gmail.com>
2024-11-01 13:19:33 +0000

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .comum import *
 DATABASES = {
     'default': config(
         'DATABASE_URL',
-        default='postgres://postgres:postgres@127.0.0.1:5432/db_solar',
+        default='postgres://postgres:postgres@127.0.0.1:5432/quebrei',
         cast=db_url
     ),
 }
